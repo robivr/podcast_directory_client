c29c4a34708d38460ab9cb4a8996cfe0a16acc26
robivr <vrbarobi@gmail.com>
2022-09-27 12:32:12 +0200

    added app logo

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Link to="/">
-          <h1 className="text-3xl font-bold">Podcast Directory</h1>
+          <div className="flex items-center justify-center">
+            <img src="/podcast_directory_logo.png" />
+          </div>
         </Link>
         <Routes>
           <Route path="/" element={<SearchView siteConfig={siteConfig} />} />
