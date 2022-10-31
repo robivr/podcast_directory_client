29ed575db398e553aaf4502efb232cfa46d5748b
robivr <vrbarobi@gmail.com>
2022-10-31 13:43:44 +0100

    put trending tiles into a grid

--- src/components/Trending/Trending.tsx
+++ src/components/Trending/Trending.tsx
@@ -19,18 +19,15 @@ const Trending = () => {
   return (
     <div>
       <h2 className="font-bold">Trending</h2>
-      <div>
+      <div className="grid grid-cols-2 lg:grid-cols-3">
         {trendingData.map((feed: any) => (
-          <div
-            className="border bg-slate-500 lg:w-1/4 p-4 rounded-xl inline-block m-2"
-            key={feed.id}
-          >
+          <div className="border p-4 rounded-xl m-2 bg-gray-600" key={feed.id}>
             <img
               className="rounded-xl"
               src={feed.artwork}
               alt={feed.title + ' artwork'}
             />
-            <p className="text-white">{feed.title}</p>
+            <p className="text-white mt-2">{feed.title}</p>
             <p className="text-white">{feed.author}</p>
           </div>
         ))}
